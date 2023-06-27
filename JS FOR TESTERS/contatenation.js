4491bf48d42f91f4db0eeb3046f1ea21b41e27d8
Fadeke <fadegboye59@gmail.com>
2023-06-27 14:21:50 +0100

    added task 5 of the module 3A

---
+++ JS FOR TESTERS/contatenation.js
@@ -0,0 +1,7 @@
+const currentDayOfTheWeek = "Tuesday";
+
+const currentDayInTheDaysOfTheWeek = 2;
+
+const output = 'Today is' + ' ' + currentDayOfTheWeek + '.' + ' ' + 'It is the' + ' ' +currentDayInTheDaysOfTheWeek + 'nd' + ' ' +'day of the week' + '.';
+
+console.log(output);

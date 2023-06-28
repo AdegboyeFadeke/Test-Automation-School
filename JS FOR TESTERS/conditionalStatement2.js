b04c12ff00ae909760f2ed9ba324789f5ebfd047
Fadeke <fadegboye59@gmail.com>
2023-06-29 00:55:02 +0100

    conditional statement2 has been added

---
+++ JS FOR TESTERS/conditionalStatement2.js
@@ -0,0 +1,29 @@
+const side1 = 2;
+const side2 = 3;
+const side3 = 4;
+const side4 = 2;
+const side5 = 2;
+
+if (side1, side2 && side3 === 2){
+    console.log("This is an Equilateral triangle")
+}else if ( side1 === side4){
+    console.log("This is an Isosceles triangle")
+}else {
+    console.log("This is Scalene triangle")
+};
+
+if (side1, side4 && side5 === 2){
+    console.log("This is an Equilateral triangle")
+}else if ( side1 === side2){
+    console.log("This is an Isosceles triangle")
+}else {
+    console.log("This is Scalene triangle")
+};
+
+if (side1, side2 && side3 === 2){
+    console.log("This is an Equilateral triangle")
+}else if ( side1 === side2){
+    console.log("This is an Isosceles triangle")
+}else {
+    console.log("This is Scalene triangle")
+};

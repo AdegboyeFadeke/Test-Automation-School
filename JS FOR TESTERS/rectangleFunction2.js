f68aa81ff979e92d4dec775b2f9c020176f8ae05
Fadeke <fadegboye59@gmail.com>
2023-06-29 02:24:44 +0100

    added the area of rentangle with return value 2

---
+++ JS FOR TESTERS/rectangleFunction2.js
@@ -0,0 +1,9 @@
+
+function calculateAreaOfRectangle2(length, width) {
+    const areaOfRectangle2 = length * width;
+    return areaOfRectangle2;
+}
+
+const result = calculateAreaOfRectangle2(7,3);
+
+console.log(result);

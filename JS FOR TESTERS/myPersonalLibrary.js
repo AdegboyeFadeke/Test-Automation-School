f63bb1aeca357801bc1d71dc1c5aaa446a229d66
Fadeke <fadegboye59@gmail.com>
2023-07-07 10:26:46 +0100

    added an toggle status to my books object

--- JS FOR TESTERS/myPersonalLibrary.js
+++ JS FOR TESTERS/myPersonalLibrary.js
@@ -3,11 +3,19 @@
     description: "Christain book",
     numberOfPages: 400,
     author: "Chris Segun Onayinka",
-    reading: false,
+    isReading: false,
+    toggleReadingStatus: function(){
+        if(books.isReading === true){
+            books.isReading = false
+        }else {
+            books.isReading = true
+        }
+    }
  }
 
 //  console.log(books['author']);
 //  console.log(books['numberOfPages']);
 //  console.log(books['description']);
 //  console.log(books['title']);
-//  console.log(books.reading);
+books.toggleReadingStatus()
+ console.log(books.isReading);

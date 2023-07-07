c01f9d8720da7f930e4fe8bfd35f37b12090151f
Fadeke <fadegboye59@gmail.com>
2023-07-07 08:27:17 +0100

    added an object literal named books

---
+++ JS FOR TESTERS/myPersonalLibrary.js
@@ -0,0 +1,13 @@
+ const books = {
+    title: "This is Our God",
+    description: "Christain book",
+    numberOfPages: 400,
+    author: "Chris Segun Onayinka",
+    reading: false,
+ }
+
+//  console.log(books['author']);
+//  console.log(books['numberOfPages']);
+//  console.log(books['description']);
+//  console.log(books['title']);
+//  console.log(books.reading);

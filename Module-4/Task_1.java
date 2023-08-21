59557c9ccc11390f35ac480955c533c7c72a1a24
Fadeke Adegboye <fadegboye59@gmail.com>
2023-08-21 16:16:01 +0100

    added task 1 module 4

--- Module-4/Task_1.java
+++ Module-4/Task_1.java
@@ -2,6 +2,6 @@ public class Task_1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello, this is my first java project");
-        System.out.println("come, let us start this journey together 0490555018");
+        System.out.println("come, let us start this journey together");
     }
 }

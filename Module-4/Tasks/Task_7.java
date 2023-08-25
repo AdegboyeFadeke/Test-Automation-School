f752d31768f2c0e192729101ae2019632554a663
Fadeke Adegboye <fadegboye59@gmail.com>
2023-08-25 11:07:56 +0100

    added task 6

---
+++ Module-4/Tasks/Task_7.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+import java.util.Arrays;
+
+public class Task_7 {
+    public static void main(String[] args) {
+        String [] [] fruits = new String [4] [3];
+        fruits [0] [0] = "Mango";
+        System.out.println(Arrays.deepToString(fruits));
+    }
+}

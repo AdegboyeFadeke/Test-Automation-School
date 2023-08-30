bc428241f89c2a29cda71496f797be8807e1a116
Fadeke Adegboye <fadegboye59@gmail.com>
2023-08-30 16:05:40 +0100

    added task 11

---
+++ Module-4/Tasks_11/Class_B.java
@@ -0,0 +1,11 @@
+package Tasks_11;
+
+public class Class_B {
+    public static void main(String[] args) {
+        Class_A fadekeVariable = new Class_A();
+        fadekeVariable.name = "fadeke";
+
+        fadekeVariable.myDetails(fadekeVariable.name);
+
+    }
+}

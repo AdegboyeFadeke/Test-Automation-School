e4095c11af75e94bed6e27c87fceddd74fdeca2f
Fadeke Adegboye <fadegboye59@gmail.com>
2023-08-31 17:06:10 +0100

    added task 13

---
+++ Module-4/Tasks_13/Class_B.java
@@ -0,0 +1,13 @@
+package Tasks_13;
+
+public class Class_B {
+    public static void main(String[] args) {
+        Class_A testifyAcademy = new Class_A("testify", 60, true);
+        testifyAcademy.printClass_A();
+
+    }
+
+
+
+
+}
